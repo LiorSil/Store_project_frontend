d02bc7c2f6811ccef6138146000f9932418fdeb3
Lior Silman <lior.silmansoad@gmail.com>
2024-07-20 08:11:04 +0300

    Update DevMode dependencies

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 require("./Configurations/db.js");
 
 const port = 5000;
@@ -10,6 +11,8 @@ const app = express();
 require("./Configurations/firebase.config.js");
 
 app.use(bodyParser.json());
+//cors
+app.use(cors());
 app.use(cookieParser());
 app.use("/favicon.ico", (req, res) => res.status(204).send());
 
